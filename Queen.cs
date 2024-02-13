81c07b816f2326ed972f40ced7d8c375daf40e34
adeolaqueen18 <159934965+adeolaqueen18@users.noreply.github.com>
2024-02-13 17:35:28 +0100

    Add files via upload

---
+++ Queen.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+namespace Solution{
+    class Solution { 
+	    static void Main(String[] args) { 
+		    int i = 4; 
+			double d = 4.0; 
+			string s = "Polyilaro ";
+			int abc;
+            
+			double def;
+            
+			string ghi;
+
+			abc = Convert.ToInt32(Console.ReadLine());
+		
+			def = Convert.ToDouble(Console.ReadLine());
+	
+			ghi = Console.ReadLine();
+            
+			Console.WriteLine(i + abc);
+            
+			Console.WriteLine(string.Format("{0:0.00}", (d + def)));
+            
+			Console.WriteLine(s + ghi);
+		}
+	}
+}
